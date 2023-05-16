f314fc22477f0d7b8995418c819ca78f10188858
anwar <anwarkp.qw@gmail.com>
2023-05-16 19:53:11 +0530

    add all files in libraray managment

---
+++ WebApplication2/adminpublishermanagment.aspx.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+
+namespace WebApplication2
+{
+    public partial class adminpublishermangment : System.Web.UI.Page
+    {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GridView1.DataBind();
+        }
+        //go button
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            getPublisherByID();
+        }
+
+        //add button
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (checkIfPublisherExists())
+            {
+                Response.Write("<script>alert('Publisher with this ID already Exist. You cannot add another Publisher with the same Publisher ID');</script>");
+            }
+            else
+            {
+                addNewPublisher();
+            }
+        }
+        //update btn
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            if (checkIfPublisherExists())
+            {
+                updatePublisher();
+
+            }
+            else
+            {
+                Response.Write("<script>alert('Publisher does not exist');</script>");
+            }
+        }
+        //delete btn
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            if (checkIfPublisherExists())
+            {
+                deletePublisher();
+
+            }
+            else
+            {
+                Response.Write("<script>alert('publisher does not exist');</script>");
+            }
+        }
+
+
+        // user defined fngetAuthorByID
+        void getPublisherByID()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("SELECT * from publisher_master_tbl where publisher_id='" + TextBox1.Text.Trim() + "';", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count >= 1)
+                {
+                    TextBox2.Text = dt.Rows[0][1].ToString();
+                }
+                else
+                {
+                    Response.Write("<script>alert('Invalid Publisher ID');</script>");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+
+            }
+        }
+
+        //fn deletePublisher
+
+        void deletePublisher()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("DELETE from publisher_master_tbl WHERE publisher_id='" + TextBox1.Text.Trim() + "'", con);
+
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<script>alert('Publisher Deleted Successfully');</script>");
+                clearForm();
+                GridView1.DataBind();
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        //fn updateAuthor
+        void updatePublisher()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("UPDATE publisher_master_tbl SET publisher_name=@publisher_name WHERE publisher_id='" + TextBox1.Text.Trim() + "'", con);
+
+                cmd.Parameters.AddWithValue("@publisher_name", TextBox2.Text.Trim());
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<script>alert('Publisher Updated Successfully');</script>");
+                clearForm();
+                GridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+        //addNewAuthor
+        void addNewPublisher()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("INSERT INTO publisher_master_tbl(publisher_id,publisher_name) values(@publisher_id,@publisher_name)", con);
+
+                cmd.Parameters.AddWithValue("@publisher_id", TextBox1.Text.Trim());
+                cmd.Parameters.AddWithValue("@publisher_name", TextBox2.Text.Trim());
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<script>alert('New publisher added Successfully');</script>");
+                clearForm();
+                GridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+        bool checkIfPublisherExists()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("SELECT * from publisher_master_tbl where publisher_id='" + TextBox1.Text.Trim() + "';", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count >= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+                return false;
+            }
+        }
+
+        void clearForm()
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+        }
+       
+    }
+}
---
+++ WebApplication2/userlogin.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+
+namespace WebApplication2
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+        // user login
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               
+
+                string username=TextBox1.Text.Trim();
+                string password=TextBox2.Text.Trim();
+
+                SqlConnection con = new SqlConnection(strcon);
+                con.Open();
+
+
+            string query = "SELECT COUNT(*) FROM member_master_tbl  WHERE member_id = @Username AND password = @Password";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@Username", username);
+            cmd.Parameters.AddWithValue("@Password", password);
+                int count =(int)cmd.ExecuteScalar();
+             if (count>0)
+                {
+                    Session["LoggedIn"]=true;
+                    Response.Redirect("userprofile.aspx");
+                }
+             else
+                {
+                    Response.Write("<script>alert('Invalid credentials');</script>");
+                }
+
+
+            }
+        catch (Exception ex) {
+                Response.Write("An error occurred: " + ex.Message);
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            
+            Response.Redirect("signuppage.aspx");
+        }
+    }
+}
+
+
+
+   
